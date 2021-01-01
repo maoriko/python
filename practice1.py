9b6ff0d697ad6b31177ebbde2279d079525076fd
moriko <maor.paz@pc4us.co.il>
2021-01-01 12:15:26 +0200

    the first practice

---
+++ practice1.py
@@ -0,0 +1,11 @@
+"""""
+Write a program with 2 variables num1, num2 with value for your choice, and one variable to hold the result
+whom get the sum of the 2 variables you put before.
+print the text the result is: and then the result. 
+"""""
+
+num1 = 100
+num2 = 200
+result = num1+num2
+print("The result is:", result)
+
