02538d318eb2eee5d8e6b74a652e7afc9c3e18cb
root <root@Monster.localdomain>
2021-01-09 13:57:28 +0200

    Add while loop to compare between numbers

---
+++ Loops/WhileLoop/practice6.py
@@ -0,0 +1,27 @@
+"""
+Write a program to get 2 values from user.
+A high value
+a low value
+print the whole range
+check if the higher value is bigger then the lower value and print accordingly
+"""
+
+high_value = int(input("Please enter a value: \n"))
+low_value = int(input("Please enter a second value: \n"))
+
+print("The first value you choose is: ", high_value)
+print("The second value you choose is: ", low_value)
+
+
+for number in range(high_value, low_value):
+    print("the range between the numbers", high_value, low_value, "is", number)
+    pass
+
+for number in range(low_value, high_value):
+    print("the range between the numbers", low_value, high_value, "is", number)
+    pass
+
+if high_value > low_value:
+    print("The higher value is:", high_value)
+else:
+    print("The higher value is:", low_value)
