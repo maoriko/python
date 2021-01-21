7007e36d97d8280ee5c791fc91fe837844988a68
maoriko <maor.paz@pc4us.co.il>
2021-01-21 17:16:29 +0200

    Mistake fixing

--- guess/guess.py
+++ guess/guess.py
@@ -2,26 +2,27 @@ import random
 
 print("Welcome to the guess game! \nPlease Enter 2 numbers ")
 minVal = (int(input("Enter the low range: ")))
-maxVal = (int(input("Enter thr max range: ")))
+maxVal = (int(input("Enter the max range: ")))
 
-n = random.randint(minVal, maxVal)
-print(n)
+rand_num = random.randint(minVal, maxVal)
+print(rand_num)
 
-count = 0
+count = 5
 guess_user_input = -1
 
-while guess_user_input != n and count < 5:
+while guess_user_input != rand_num and count > 0:
     guess_user_input = (int(input("Guess your number: ")))
 
-    if guess_user_input == n:
-        print("Great!\n you guessed after", count + 1, "times")
+    if guess_user_input == rand_num:
+        print("Great!\nYou guessed after", count - 1, "times")
         break
 
-    elif guess_user_input < n:
-        print("Try bigger number")
+    elif guess_user_input < rand_num:
+        print("Try bigger number\nYou have", count - 1, "times left")
 
-    elif guess_user_input > n:
-        print("Try a smaller number")
-    else:
-        print("try again")
-    count += 1
+    elif guess_user_input > rand_num:
+        print("Try smaller number\nYou have", count - 1, "times left")
+    count -= 1
+
+else:
+    print("Better luck next time! ")
