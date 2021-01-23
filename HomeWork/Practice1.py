6a3d4f7537676cc5101e675d401704255b749275
maoriko <maor.paz@pc4us.co.il>
2021-01-23 12:50:44 +0200

    Fix homework!!!!

--- HomeWork/Practice1.py
+++ HomeWork/Practice1.py
@@ -3,8 +3,8 @@ Date: 22/01/2021
 Author: Maor Paz
 """
 import random
+arr = [0, 1000]
 
-arr = [0, 100, 1000]
 
 # Define the Maximum function
 def maximum_func(nums):
@@ -14,6 +14,7 @@ def maximum_func(nums):
             my_max = i
     return my_max
 
+
 # Define the Minimum function
 def minimum_func(nums):
     my_min = 0
@@ -21,6 +22,7 @@ def minimum_func(nums):
         if i < my_min:
             my_min = i
     return my_min
+
 
 while True:
     print(
@@ -47,88 +49,87 @@ while True:
 
     # Add multiple numbers or 1
     if user_input == '1':
-        sure = -1
-        another = -1
-        while sure != 'n' or another != 'n':
-            add_num = int(input("\nAdd number to list: "))
-            print("You choose to insert", add_num, "to your list:", arr)
-            sure = input("\nAre you sure you want to insert this number? y/n: ")
 
-            if sure == 'y':
-                num_to_add = arr.append(add_num)
-                print("\nA number", add_num, "added to your list:", arr, "\n You like to add another?")
+        while True:
+            try:
+                user_input = int(input("\nAdd number to list: "))
+            except ValueError:
+                print("This is not allowed here!")
+                break
 
-                user_input = input("Enter y/n: ")
-                while user_input != 'y':
+            print("You choose to insert", user_input, "to your list:", arr)
+            ask = input("\nAre you sure you want to insert this number? y/n: ")
+            if ask == 'y':
+                arr.append(user_input)
+                print("\nA number", user_input, "added to your list:", arr)
+            elif ask != 'y':
+                print("\nI don't know what to do, going to main menu!")
+                user_input = input("\nWould you like to add another? y/n ")
+                if user_input == 'y':
+                    continue
+                elif user_input == 'n':
+                    break
+                else:
+                    print("\nNot Allowed going to top")
+                    break
+        # else:
+        print("\nReturning to main menu")
+        continue
 
-                    if user_input == 'n':
-                        print("\nGoing to the main menu")
-                        break
-
-                    elif user_input != 'y' or user_input != 'n':
-                        print("\nNot Allowed! type y / n")
-
-                    else:
-                        break
-                continue
-            continue
-        else:
-            break
-
-# Remove from list.
+    # Remove from list.
     if user_input == '2':
         print("This is your list:", arr)
         user_input = int(input("What number would you like to remove: "))
-        # print(arr)
+
         while True:
             if user_input in arr:
                 print("the number", user_input, "successfully Removed")
                 arr.remove(user_input)
                 print(arr)
                 break
+
             elif user_input not in arr:
                 print("\n The number: ", user_input, "does not exist!, Try again")
                 break
             continue
 
-# Display min Value
+    # Display min Value
     if user_input == '3':
         print("\nThe minimum number is:\n", minimum_func(arr))
 
-# Display max Value
+    # Display max Value
     if user_input == '4':
         print("The maximum value in the list is:\n", maximum_func(arr))
 
-
-# Average of the numbers
+    # Average of the numbers
     if user_input == '5':
         for i in arr:
             i += 1
             count = sum(arr)
             print("The average is", count / i)
 
-# Print all numbers within the list
+    # Print all numbers within the list
     if user_input == '6':
         i = 1
         while maximum_func(arr) > i > minimum_func(arr):
             print(i)
             i += 1
 
-# Sort list min to max
+    # Sort list min to max
     if user_input == '7':
         print("\nYou choose to sort you list from min to max\n")
         print("Here is your sotred list", sorted(arr))
 
-# Sort from max to min
+    # Sort from max to min
     if user_input == '8':
         print("\nYou choose to sort you list from max to min\n\n")
         print(sorted(arr, reverse=True))
 
-# Clear all the list and insert 500 random numbers
+    # Clear all the list and insert 500 random numbers
     if user_input == '9':
         print("Your list is cleared!")
-        arr = [random.randint(0, 500)]
+        arr = [random.randint(0, 501)]
         i = 1
-        while i < 500:
+        while i < 501:
             print(i)
             i += 1
