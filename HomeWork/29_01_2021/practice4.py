c546ac24d8f5e67e066d56c89bc55b727c35bd43
maoriko <maor.paz@pc4us.co.il>
2021-02-04 23:00:15 +0200

    Added HomeWorks! practice 4

--- HomeWork/29_01_2021/practice4.py
+++ HomeWork/29_01_2021/practice4.py
@@ -1,67 +1,79 @@
-s = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+, -./:;<=>?@[\]^_`{|}~"
-low_case = s[0:26]
-numbers = s[26:36]
-capital = s[36:62]
-special = s[62:]
-score = 0
+def score_total(password):
+    s = "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'()*+, -./:;<=>?@[\]^_`{|}~"
+    low_case = s[0:26]
+    s_low = 0
+    numbers = s[26:36]
+    s_nums = 0
+    capital = s[36:62]
+    s_cap = 0
+    special = s[62:]
+    s_special = 0
+    len_points = 0
+    len_letters = len(password)
+    bonus_point = 0
 
-password = input('Enter your password > ')
+    while True:
+        if len_letters < 6:
+            len_points += 1
+            break
+        elif len_letters == 6 or len_letters == 7 or len_letters == 8:
+            len_points += 2
+            break
+        elif len_letters > 8:
+            len_points += 3
+            break
 
+    for i in passwd:
+        if i in low_case:
+            s_low += 1
+            break
+    for i in passwd:
+        if i in numbers:
+            s_nums += 1
+            break
+    for i in passwd:
+        if i in capital:
+            s_cap += 1
+            break
+    for i in passwd:
+        if i in special:
+            s_special += 2
+            break
 
-def find_password_strength(password):
-    password_length = len(password)
-    password_length_score = 0
-    password_strength = 0
-    lowercase_count = 0
-    uppercase_count = 0
-    numbers_count = 0
-    special_count = 0
-    bonus_count = 0
-    weak_password = 4
-    medium_password = 6
-    strong_password = 7
-    very_strong_password = 8
+    if password in special and password in capital or low_case or numbers:
+        bonus_point += 1
+    elif password in special and password in capital and password in special or numbers:
+        bonus_point += 2
 
+    total = s_low + s_nums + s_cap + s_special + len_points + bonus_point
 
-    if password_length < 6:
-        password_length_score += 1
-    while password_length in range(6, 8):
-        password_length_score += 2
-        break
-    else:
-        password_length_score += 3
-
-    for char in password:
-        password_strength += 1
-        if char in low_case:
-            lowercase_count += 1
-        if char in capital:
-            uppercase_count += 1
-        if char in numbers:
-            numbers_count += 1
-        if char in special:
-            special_count += 2
-        if char in special and char in low_case or char in numbers or char in capital:
-            bonus_count += 2
-
-    character_sum = (
-                lowercase_count + uppercase_count + numbers_count + special_count + bonus_count + password_length_score)
-    print(character_sum)
-
-    password_strength += character_sum
-    print('Your passwords score was: ', password_strength)
-
-    if weak_password <= password_strength < strong_password:
-        print('Password strength is weak.')
-
-    elif password_strength < medium_password:
-        print('Password strength is medium.')
-
-    elif password_strength >= strong_password:
-        print('Password strength is Strong.')
-
-    elif password_strength >= very_strong_password:
-        print("Password strength is very strong")
+    if total <= 4:
+        print("Your password is weak.")
+    elif total == 5 or total == 6:
+        print("Your password is medium")
+    elif total == 7:
+        print("Your password strong")
+    elif total > 8:
+        print("You password is very strong")
+    return total
 
 
-find_password_strength(password)
+while True:
+    print("In front of you a password checker, Here is the score table:\n"
+          "4 points = Weak password\n"
+          "5-6 points = Medium password\n"
+          "7 points = Strong password\n"
+          "8 points and above = Very strong password\n ")
+    passwd = input("To check you password score type it here: ")
+    print("Total Score is: ", score_total(passwd))
+
+    try:
+        start_again = input("would you like to check another password? y/n: ")
+        if start_again == 'y' or start_again == 'Y':
+            continue
+        elif start_again == 'n' or start_again == 'N':
+            print('Bye!')
+            break
+    except ValueError:
+        print("Unknown command")
+        continue
