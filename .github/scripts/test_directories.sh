7081909f8b7913fa9a317208ad8aaf0934a7ad9e
MaorPaz <maor.p@akeyless.io>
2021-05-02 15:51:25 +0300

    Added user names akeylesszt for zero trust access to databases

---
+++ .github/scripts/test_directories.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+set -e
+set -x
+
+_USERS='dscl . list /Users | grep -v "^_"'
+UHOME=/Users/"${_USERS}"
+
+for user in $_USERS; do
+	_dir=_dir="${UHOME}/${_USERS}"
+
+	if [ -d "$_dir" ]; then
+
+		cat << EOF > ~/.akeyless-sphere.rc
+		identity_file=""
+		cert_issuer_name="akeyless-zero-trust/staging/cert-issuer"
+		profile="akeyless-zero-trust"
+		BASTION_API_PROTO_=https
+EOF
+
+chmod 444 ~/{"${UHOME}"}/.akeyless-sphere.rc
+
+
+&&
+		cat << EOF > ~/.akeyless/profiles/akeyless-zero-trust.toml
+		["default"]
+		access_id = "p-6lpq3mwugkqv"
+  		access_type = "saml"
+EOF
+
+chmod 444 ~/"${UHOME}"/.akeyless/profiles/akeyless-zero-trust.toml
+	fi
+
+done
+
+cat  ~/"${UHOME}"/.akeyless/profiles/akeyless-zero-trust.toml
+cat ~/{"${UHOME}"}/.akeyless-sphere.rc
