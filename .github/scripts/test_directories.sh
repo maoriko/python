e25750b996d181a59e9b504b76705b0994bd4dcc
MaorPaz <maor.p@akeyless.io>
2021-05-02 17:22:18 +0300

    Added user names akeylesszt for zero trust access to databases

--- .github/scripts/test_directories.sh
+++ .github/scripts/test_directories.sh
@@ -15,16 +15,16 @@ EOF
     fi
 done
 cat $_dir/.test-akeyless-sphere.rc
-
-for user in $_USERS; do
-    _dir=/Users/$user
-    if [ -d "$_dir" ]; then
-		  cat << EOF > $_dir/.akeyless/profiles/test-akeyless-zero-trust.toml
-		  ["akeyless-zero-trust"]
-		  access_id = "p-6lpq3mwugkqv"
-  		access_type = "saml"
-EOF
-      chmod 444 $_dir/.akeyless/profiles/test-akeyless-zero-trust.toml
-    fi
-done
-cat $_dir/.akeyless/profiles/test-akeyless-zero-trust.toml
+#
+#for user in $_USERS; do
+#    _dir=/Users/$user
+#    if [ -d "$_dir" ]; then
+#		  cat << EOF > $_dir/.akeyless/profiles/test-akeyless-zero-trust.toml
+#		  ["akeyless-zero-trust"]
+#		  access_id = "p-6lpq3mwugkqv"
+#  		access_type = "saml"
+#EOF
+#      chmod 444 $_dir/.akeyless/profiles/test-akeyless-zero-trust.toml
+#    fi
+#done
+#cat $_dir/.akeyless/profiles/test-akeyless-zero-trust.toml
