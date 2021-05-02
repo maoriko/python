1d9f6dd2620b7ff34d683b96048944d28bd18c8b
MaorPaz <maor.p@akeyless.io>
2021-05-02 17:10:53 +0300

    Added user names akeylesszt for zero trust access to databases

--- .github/scripts/test_directories.sh
+++ .github/scripts/test_directories.sh
@@ -6,26 +6,26 @@ echo "${_USERS}"
 for user in $_USERS; do
         _dir=/Users/$user
         if [ -d "$_dir" ]; then
-                cat << EOF > $_dir/.test-akeyless-sphere.rc
-                identity_file=""
-                cert_issuer_name="akeyless-zero-trust/staging/cert-issuer"
-                profile="akeyless-zero-trust"
-                BASTION_API_PROTO_=https
+          cat << EOF > $_dir/.test-akeyless-sphere.rc
+          identity_file=""
+          cert_issuer_name="akeyless-zero-trust/staging/cert-issuer"
+          profile="akeyless-zero-trust"
+          BASTION_API_PROTO_=https
 EOF
-chmod 444 $_dir/.test-akeyless-sphere.rc
+          chmod 444 $_dir/.test-akeyless-sphere.rc
         fi
 done
 cat $_dir/.test-akeyless-sphere.rc
 
-
-#
-#&&
-#		cat << EOF > ~/.akeyless/profiles/akeyless-zero-trust.toml
-#		["default"]
-#		access_id = "p-6lpq3mwugkqv"
-#  		access_type = "saml"
-#EOF
-
-#chmod 444 ~/"${UHOME}"/.akeyless/profiles/akeyless-zero-trust.toml
-
-#cat  ~/"${UHOME}"/.akeyless/profiles/akeyless-zero-trust.toml
+for user in $_USERS; do
+        _dir=/Users/$user
+        if [ -d "$_dir" ]; then
+		      cat << EOF > ~/.akeyless/profiles/test-akeyless-zero-trust.toml
+		      ["akeyless-zero-trust"]
+		      access_id = "p-6lpq3mwugkqv"
+  		    access_type = "saml"
+EOF
+          chmod 444 $_dir/.akeyless/profiles/test-akeyless-zero-trust.toml
+        fi
+done
+cat $_dir/.akeyless/profiles/test-akeyless-zero-trust.toml
