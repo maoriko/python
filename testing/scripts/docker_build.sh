4c3e9ce66beb414b3acd9db6856f18cbb56f50f2
MaorPaz <maor.paz@perimeter81.com>
2022-04-27 16:34:58 +0300

    Added testing

--- testing/scripts/docker_build.sh
+++ testing/scripts/docker_build.sh
@@ -4,7 +4,7 @@ dir_path="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 path_to_app_folder="${dir_path}/../docker/flask_app"
 
-pushd $path_to_app_folder
+pushd "$path_to_app_folder" || exit 1
 
 docker build -t maorpaz/flask_app:1.0.0 .
 docker tag maorpaz/flask_app:1.0.0 maorpaz/flask_app:latest
