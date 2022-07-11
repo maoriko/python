590dc9d8b9811c10a7ad9963e56ddc204cf3163d
MaorPaz <maor.paz@perimeter81.com>
2022-07-11 17:38:44 +0300

    ip_adress_check

---
+++ AWS/attached_ips/ip_finder.py
@@ -0,0 +1,43 @@
+import boto3
+
+final_eip = {}
+eips = [
+    "XXX.XXX.XXX.XXX"
+]
+
+print(len(eips))
+ec2_client = boto3.client('ec2')
+
+# Retrieves all regions/endpoints that work with EC2
+response = ec2_client.describe_regions()
+region_list = response['Regions']
+
+
+def ip_address_finder(region):
+    c = 0
+    client = boto3.client('ec2', region_name=region["RegionName"])
+    try:
+        addresses_dict = client.describe_addresses()
+
+        for eip_dict in addresses_dict['Addresses']:
+            ip = eip_dict['PublicIp']
+            if ip in eips:
+                instance_id = ""
+                if eip_dict.get('InstanceId'):
+                    instance_id = eip_dict.get('InstanceId')
+                c += 1
+                print(f"ip - {ip} instance id = {instance_id}")
+    except:
+        pass
+
+
+for region in region_list:
+    print("Region %s " % region['RegionName'])
+    print("===========================================\n")
+    reg = region['RegionName']
+
+    ip_address_finder(region)
+
+    print("===========================================\n")
+print('Done.')
+
