3b4ea115ba12659ce1195eff6e0cc9c3766747fa
MaorPaz <pc4uss@gmail.com>
2022-11-05 13:41:17 +0200

    update README.md

--- slackbot/slack_bot_app/slackbot.py
+++
@@ -1,5 +0,0 @@
-import os
-import time
-import re
-from slackclient import SlackClient
-
---
+++ slackbot/slack_bot_app/slackbot/slackbot.py
@@ -0,0 +1,12 @@
+# import os
+# import time
+# import re
+# from slackclient import SlackClient
+#
+# SLACK_BOT_TOKEN = ""
+#
+# slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+#
+# print("hello world!")
+
+
